bbe581b04c989d561d49d6ae301b6bcfe6691242
staydecent <dev@staydecent.ca>
2012-02-09 14:57:50 -0800

    Added Event class and comments. All classes extend a singleton

--- Gum.php
+++ Gum.php
@@ -6,29 +6,103 @@
  * By Adrian Unger <http://staydecent.ca>
  * Public Domain or something.
  */
-
 namespace Gum;
 
-class InvalidRequestMethod extends \Exception {}
-class InvalidRequestRoute extends \Exception {}
-
-class Stick {
-
+/**
+ * Singleton
+ *
+ * @package Gum
+ */
+class Single {
+    
     private static $instance;
 
-    private $rules = array();
-    private $route;
+    /**
+     * Get the instance.
+     *
+     * @return Object
+     */
+    public static function get_instance() 
+    {
+        if ( ! isset(self::$instance)) 
+        {
+            self::$instance = new self();
+        }
 
-    public $is_matched = FALSE;
+        return self::$instance;
+    }
+}
 
-    public static function delegate()
+/**
+ * Provided by Danillo César de O. Melo
+ * https://github.com/danillos/fire_event/blob/master/Event.php
+ */
+class Event extends Single {
+  
+    /**
+     * Hooks are pairs of events => functions to call.
+     *
+     * @var array
+     */
+    private $hooks = array();
+  
+    /**
+     * Add a function to an event.
+     *
+     * @param  string $event_name the name of the event to hook into
+     * @param  string $fn the function to call
+     * @return void
+     */
+    public static function hook($event_name, $fn) 
+    {
+        $instance = self::get_instance();
+        $instance->hooks[$event_name][] = $fn;
+    }
+  
+    /**
+     * Trigger/fire/invoke an event, calling all hooked functions.
+     *
+     * @param  string $event_name the name of the event to invoke
+     * @param  mixed  $params Parameters to pass to the hooked functions
+     * @return void
+     */
+    public static function fire($event_name, $params = NULL) 
     {
         $instance = self::get_instance();
 
-        if ($instance->is_matched)
+        if (array_key_exists($event_name, $instance->hooks)) 
         {
-            return TRUE;
+            foreach ($instance->hooks[$event_name] as $fn) 
+            {
+                if (is_array($fn) || is_string($fn))
+                {
+                    call_user_func_array($fn, array(&$params));
+                }
+            }
         }
+    }
+}
+
+/**
+ * HTTP Router
+ *
+ * @package Gum
+ */
+class Route extends Single {
+
+    private $rules = array(),
+            $route;
+
+    public  $is_matched = FALSE;
+
+    /**
+     * Matches the current route to a rule, invoking the callback.
+     *
+     * @return void
+     */
+    public static function delegate()
+    {
+        $instance = self::get_instance();
 
         foreach ($instance->rules as $rule => $callback) 
         {
@@ -43,8 +117,20 @@ class Stick {
         }  
     }
 
+    /**
+     * is triggered when invoking inaccessible methods in a static context.
+     *
+     * @param  string $name name of the request method
+     * @param  array  $args
+     * @return void
+     */
     public static function __callStatic($name, $args)
     {
+        if ($instance->is_matched)
+        {
+            return TRUE;
+        }
+        
         if ($name === strtolower($_SERVER['REQUEST_METHOD']))
         {
             $instance = self::get_instance();
@@ -55,19 +141,14 @@ class Stick {
         }
     }
 
+    /**
+     * See if we've got a match.
+     *
+     * @return bool
+     */
     public static function not_found()
     {
         $instance = self::get_instance();
         return ($instance->is_matched) ? FALSE : TRUE;
-    }
-
-    public static function get_instance() 
-    {
-        if ( ! isset(self::$instance)) 
-        {
-            self::$instance = new Stick();
-        }
-
-        return self::$instance;
     }
 }
